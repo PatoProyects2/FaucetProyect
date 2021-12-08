60fb6cbd203621d7a4b49ce88c445b4bd16cd9f8
PatoVerde <leandrodenos@gmail.com>
2021-12-08 05:14:31 -0300

    No te quemes pato

--- contracts/nfts/smithy.sol
+++ contracts/nfts/smithy.sol
@@ -2,39 +2,42 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";  
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+
 //import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract smiti is ERC721, Ownable {
+contract SMITHY is  ERC721, ERC721Enumerable, Ownable{
 
     //using SafeERC20 for IERC20;
     uint256 COUNTER = 1;
     uint256 fee = 1 ether;
     uint256 costMint = 50 ether; 
     address feeWallet;
-    IERC20 buyTokenMinter;//Addres of token used to mint nfts.
+    IERC20 buyTokenMinter;//Address del token que paga el user por hacer mint
     
 
     struct smithy{
-        string Name; //delete this var
+        string name; //delete this var
         uint256 id;
         uint256 dna; //why this var
-        uint256 Serie; 
-        uint8 level;
-        uint8 stars; // This var is for more chance to upgrade your level
-        uint256 energy; // Esta var es para mantener la produccion del staking.   
+        uint256 serie; 
+        uint256 energy; // Esta var es para mantener la produccion del staking.
+        uint256 efficiency; // Esta var es para mantener la produccion del staking.
+        uint256 level;
+        uint256 stars; // This var is for more chance to upgrade your level
+        
     }
     //Info of token need to update stats.
     struct smithyTokenInfo{
         IERC20 levelUPToken; //Addres to level up smithy
         IERC20 starsUpToken; //Address to stars up smithy
-        IERC20 energyRecharge; //Address to energy recharge         
+        IERC20 energyRecharge; //Address to energy recharge   
+        // efficincy token      
     }
 
     smithy[] public smithys;
@@ -42,24 +45,26 @@ contract smiti is ERC721, Ownable {
     smithyTokenInfo public tokensUpgrade; // Info of token need to update stats.
 
     constructor(
-        string memory _name, 
-        string memory _symbol,
-        address _feeWallet,
-        IERC20 _levelUPToken,
-        IERC20 _starsUpToken,
-        IERC20 _energyRecharge,
-        IERC20 _buyTokenMinter
-    )ERC721(_name,_symbol)
+        //string memory _name, 
+        //string memory _symbol,
+        //address _feeWallet,
+        //IERC20 _levelUPToken,
+        //IERC20 _starsUpToken,
+        //IERC20 _energyRecharge,
+        //IERC20 _buyTokenMinter
+    )ERC721("SMITHY","SMTY")
     {
         //setUp tokens needs
-        feeWallet = _feeWallet;
-        tokensUpgrade.levelUPToken = _levelUPToken;
-        tokensUpgrade.starsUpToken = _starsUpToken;
-        tokensUpgrade.energyRecharge = _energyRecharge;
-        buyTokenMinter = _buyTokenMinter;
+        //feeWallet = _feeWallet;
+        //tokensUpgrade.levelUPToken = _levelUPToken;
+        //tokensUpgrade.starsUpToken = _starsUpToken;
+        //tokensUpgrade.energyRecharge = _energyRecharge;
+        //buyTokenMinter = _buyTokenMinter;
+        //eficiency token
     }
 
     event NewSmithy ( address indexed owner, uint256 id, uint256 dna);
+    event levelUp (uint256 id, uint lvl);
 
     //Helpers
     //FUNCTION TU SETTER TOKENS.
@@ -70,6 +75,7 @@ contract smiti is ERC721, Ownable {
         return randomNum % _mod;
     }
 
+    //regresa el numero de serie 0-5 (no escalable :/).
     function _getSerie() internal view returns(uint256){
         uint256 s;
         if(COUNTER < 100) {
@@ -92,36 +98,28 @@ contract smiti is ERC721, Ownable {
         }
 
         return s;
-
     }
 
     function updateFee(uint256 _fee) external onlyOwner() {
         fee = _fee;
     }
-
-    function withdraw() external payable onlyOwner(){
-        address payable _owner = payable(owner());
-        _owner.transfer(address(this).balance);
-    }
     
     //creation
     function _createSmithy(string memory _name, uint256 _serie) internal {
         uint256 randDna =  _createRandomNum(10*16);
-        smithy memory newSmithy = smithy (_name, COUNTER, randDna, _serie, 1, 1, 100);
+        smithy memory newSmithy = smithy (_name, COUNTER, randDna, _serie, 100, 10, 1, 1);
         smithys.push(newSmithy);
-        _safeMint(msg.sender, COUNTER);        
+        _safeMint(msg.sender, COUNTER);        //erc721 standar func
         emit NewSmithy(msg.sender, COUNTER, randDna);
         COUNTER++;
     }
-
     
     function createRandomSmithy (string memory _name) public payable {        
-        require(buyTokenMinter.balanceOf(msg.sender) >= costMint, "not founds for this buy" );
-        buyTokenMinter.transferFrom(address(msg.sender), address(feeWallet), costMint);
+        //require(buyTokenMinter.balanceOf(msg.sender) >= costMint, "not founds for this buy" );
+        //buyTokenMinter.transferFrom(address(msg.sender), address(feeWallet), costMint);
         uint256 serie = _getSerie();
         _createSmithy(_name, serie);
     }
-
 
     //getter and Setters
     function getSmithy() public  view returns(smithy[] memory){
@@ -133,26 +131,73 @@ contract smiti is ERC721, Ownable {
     }
     function setCostMintNFT(uint _newCostMint) public onlyOwner {
         costMint = _newCostMint;        
+    }    
+    function setTokenMinter(IERC20 _buyTokenMinter)public onlyOwner{
+        buyTokenMinter = _buyTokenMinter;
     }
-    //function setTokenMinter
 
-    //Upgrades 
+    //--------------------FUNCTIONS TO UPDATE SMITHY STATS.---------------------------
+    //---- smithys.level  functions
+
     //upgrade level
-    function upgradeLevel(uint _id) public returns(bool){
+    function upgradeLevel(address _user,uint _id) public returns(bool){
         smithy memory _smiti = smithys[_id-1];
-        require(_smiti.id == _id, "No son iguales");
+        require(_smiti.id == _id, "errore al encontrar el nft ingresado por parametro");
+        require(_user == ownerOf(_id), "Quien llama no tiene este nft" );
+        //require (msg.sender sea el dueño del nft)
         uint cost = costLevelUp(smithys[_id-1].level);
 
-        require(tokensUpgrade.levelUPToken.balanceOf(address(msg.sender)) >= cost, "No tiene fondos suficientes");
-
-        smithys[_id-1].level++; 
+        require(tokensUpgrade.levelUPToken.balanceOf(_user) >= cost, "No tiene fondos suficientes");
+        tokensUpgrade.levelUPToken.transferFrom(_user, address(feeWallet), cost);
+        smithys[_id-1].level++;
+        
             
-        //transfer cost to contractFEES wallets       
+        emit levelUp(_id,smithys[_id-1].level);
         return true;        
     }
-    //Requisite to upgrade level.
+    //Tokens que se requieren para subir 1 nivel
     function costLevelUp(uint256 _level) public pure returns(uint256){
         return 1*_level+_level**6;
+    }
+
+
+    //---- smithy.energy functions ----
+
+    //Recharge energy
+    function rechargeEnergy(address _user,uint _id) public returns(bool){
+        smithy memory _smiti = smithys[_id-1];
+        require(_smiti.id == _id, "errore al encontrar el nft ingresado por parametro");
+        require(_user == ownerOf(_id), "Quien llama no tiene este nft" );
+        uint cost= costOfRechargeEnergy(_smiti.level);
+        require(tokensUpgrade.energyRecharge.balanceOf(_user) >= cost, "no tiene fondos suficientes");
+        tokensUpgrade.energyRecharge.transferFrom(_user, address(feeWallet), cost); //$gems
+        //no te quemes pato...
+
+        return true;
+    }
+    
+    //Cantidad de tokens que se requiere para recargar energia
+    function costOfRechargeEnergy(uint _level) public pure returns(uint256){
+        return 1*_level; //editar cuando se tengan numeros de produccion / dia.
+    }
+
+
+
+    // funciones para evitar conflictos erc721 standar con erc721enumerable standar.
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
 }
