3b04914109498f7c995b91429388483272591f86
PatoVerde <leandrodenos@gmail.com>
2021-12-26 18:16:27 -0300

    se rompe sin que lo toque

--- contracts/StakeNFT.sol
+++ contracts/StakeNFT.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";    
 
 import "./nfts/smithy.sol"; // minter token of rewards
 import "./token/silver.sol";
