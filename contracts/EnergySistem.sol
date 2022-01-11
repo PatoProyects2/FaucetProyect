995b786ceb4cee902e6ed49a0abb10304fcfa525
PatoVerde <leandrodenos@gmail.com>
2022-01-11 19:26:51 -0300

    migrations refactory

---
+++ contracts/EnergySistem.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract EnergySistem {
+    IERC20 public buyToken;
+    address public owner;
+    address public feeWallet;
+    uint public pricePerDay;
+
+    // Addres del (user => fecha en segudos donde se le termina la energia al user)
+    mapping (address=> uint256) public userEnergy;
+
+    constructor (uint _price, address _feeWallet){
+        owner = msg.sender; 
+        feeWallet = _feeWallet;
+        pricePerDay = _price * 1 ether;
+    }
+    
+    function buyDay(uint _days) public returns(uint){
+        uint _price = pricePerDay * _days; // Todo: -Bug si compra dos veces seguidas-
+        require(checkUser(msg.sender) == false, "Ya tiene tiempo comprado." );
+        require(buyToken.balanceOf(msg.sender) >= _price, "Fondos insuficientes.");        
+        buyToken.transferFrom(msg.sender,feeWallet, _price);
+        userEnergy[msg.sender] = block.timestamp + _days * 1 days;
+        return (userEnergy[msg.sender] - block.timestamp);
+    }
+
+    function checkUser(address _user) public view returns(bool){
+        if(userEnergy[_user]> block.timestamp){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    function setBuyToken(address _buyToken) public {
+        require (msg.sender == owner , "NO eres el owner");
+        buyToken = IERC20(_buyToken);
+    }
+
+    //price in wai
+    function setPricePerDay (uint _pricePerDayInWai) public {
+        require (msg.sender == owner , "NO eres el owner");
+        pricePerDay = _pricePerDayInWai * 1 ether;
+    }
+ 
+        /* TODO:
+        *
+        */
+}
+
--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,58 +1,21 @@
-const PatoVerde = artifacts.require('PatoVerde');
-const Staking = artifacts.require('Staking');
-const Reward = artifacts.require('Reward');
-const Faucet = artifacts.require("Faucet");
-//const RandomToken = artifacts.require('RandomToken');
+const EnergySistem = artifacts.require('EnergySistem');
 
 module.exports = async function (deployer, network, accounts) {
-  
-  //const viellasPerBlock = web3.utils.toWei('0.1', 'ether');
-  const viellasPerBlock = web3.utils.toWei('45000000000000000', 'wei');
-  var PATO;
-  var stk;
-  var rwrd;
-  //var rnd;
-  var fau;
+  var Energy;
+  var price = web3.utils.toWei('1', 'wei'); // price in wai
 
   if(network === 'development'){
-    await deployer.deploy(PatoVerde, accounts[0]);
-    PATO = await PatoVerde.deployed();
-
-    await deployer.deploy(Reward, PATO.address);
-    rwrd = await Reward.deployed();
-
-    await deployer.deploy(Staking, PATO.address, PATO.address, accounts[0] , viellasPerBlock, 0, rwrd.address);
-    await deployer.deploy(Faucet, accounts[0], PATO.address);
+    await deployer.deploy(EnergySistem, price, accounts[1]);
+    Energy = await EnergySistem.deployed();
 
   }else{
-    //await deployer.deploy(PatoVerde, accounts[0]);
-    // = await PatoVerde.deployed();// MODIFICAR ESTO ANTES DE HACER DEPLOY.
+    await deployer.deploy(/*EnergySistem, accounts[0]*/);
+    Energy = await EnergySistem.deployed();// MODIFICAR ESTO ANTES DE HACER DEPLOY.
 
-    await deployer.deploy(Reward, PATO.address);
-    rwrd = await Reward.deployed();
-
-    await deployer.deploy(Staking, PATO.address, PATO.address, accounts[0], viellasPerBlock, 0, rwrd.address);
-    await deployer.deploy(Faucet, accounts[0], PATO.address);
   }
-/*
-  await deployer.deploy(RandomToken, web3.utils.toWei('1000000', 'ether'));
 
-  //rnd = await RandomToken.deployed(); 
-*/
-  fau = await Faucet.deployed()
-  stk = await Staking.deployed();
-
-  await rwrd.setStakingAddress(stk.address);
-  await PATO.setStakingMinter(stk.address);
+  var content = "export const " + network + "EnergyAddress = \"" + Energy.address + "\"\n";
   
-
-
-
-  var content = "export const " + network + "PatoAddress = \"" + PATO.address + "\"\n";
-  content += "export const " + network + "FaucetAddress = \"" + fau.address + "\"\n";
-  content += "export const " + network + "StakingAddress = \"" + stk.address + "\"\n";
-  //content += "export const " + network + "RandomTokenAddress = \"" + rnd.address + "\"\n";
-
   var fs = require('fs');
   fs.writeFile(network + ".js", content, (err)=>{
     if(err){
