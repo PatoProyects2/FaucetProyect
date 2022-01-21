eff604177a21ff4cc4f7c9a2d5c2b16aa1fe7d9d
guillermo <guille1000142@gmail.com>
2022-01-21 16:44:35 +0100

    juego nft

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -8,6 +8,8 @@ import {
 } from "react-router-dom"
 import styled from 'styled-components'
 
+import Footer from '../../components/Footer/Footer'
+
 import Shop from './components/Shop/Shop'
 import Elements from './components/Elements/Elements'
 import Castle from './components/Castle/Castle'
@@ -15,33 +17,42 @@ import Castle from './components/Castle/Castle'
 import Gold from '../../images/Game/gold.png'
 import Gems from '../../images/Game/gema.png'
 import Wood from '../../images/Game/wood.png'
+import Background from '../../images/Game/fondo.jpg'
 
 import './Game.css'
 
 const GameStyled = styled.div`
 margin: 0 auto;
 width: 1000px;
-left: 0;
-right: 0;
+color: #ffffff;
 `
 
 const HeaderStyled = styled.div`
 margin: 0 auto;
-color: white;
 padding: 10px;
 `
 
 const Tokens = styled.div`
 display: inline-block;
 padding: 15px;
-color: var(--color-text);
+color: #ffffff;
 `
 const GameButtons = styled.div`
+height: 500px;
 display: flex;
 flex-direction: column;
 float: left;
-position: sticky;
-margin: auto;
+margin: 0 auto;
+align-itmes: center;
+justify-content: center;
+position: relative;
+background-color: black;
+`
+const MainSection = styled.div`
+background-image: url(${Background});
+background-size: cover;
+height: 500px;
+color: #ffffff;
 `
 
 function Slide() {
@@ -58,53 +69,59 @@ function Game() {
     let { path, url } = useRouteMatch();
 
     return (
-        <GameStyled>
-            <HeaderStyled>
-                <Tokens>
-                    <img src={Gold} width="25" height="25" alt="" />
-                    &nbsp;100
-                </Tokens>
-                <Tokens>
-                    <img src={Gems} width="25" height="25" alt="" />
-                    &nbsp;400
-                </Tokens>
-                <Tokens>
-                    <img src={Wood} width="25" height="25" alt="" />
-                    &nbsp;20
-                </Tokens>
-            </HeaderStyled>
-            <GameButtons>
-                <NavLink className="off" activeClassName="on" to={`${url}/castle`}>
-                    <button
-                        id="buttonCastle"
-                    >
-                    </button>
-                </NavLink>
-                <NavLink className="off" activeClassName="on" to={`${url}/shop`}>
-                    <button
-                        id="buttonShop"
-                    >
-                    </button>
-                </NavLink>
-                <NavLink className="off" activeClassName="on" to={`${url}/elements`}>
-                    <button
-                        id="buttonBag"
-                    >
-                    </button>
-                </NavLink>
-            </GameButtons>
-            <Switch>
-                <Route exact path={path}>
-                    <h1>Start playing</h1>
-                </Route>
-                <Route path={`${path}/:slide`} component={Slide} />
-            </Switch>
-            <Switch>
-                <Route path={`${path}/castle`} component={Castle} />
-                <Route path={`${path}/shop`} component={Shop} />
-                <Route path={`${path}/elements`} component={Elements} />
-            </Switch>
-        </GameStyled>
+        <>
+            <GameStyled>
+                <GameButtons>
+                    <NavLink className="off" activeClassName="on" to={`${url}/castle`}>
+                        <button
+                            id="buttonCastle"
+                        >
+                        </button>
+                    </NavLink>
+                    <NavLink className="off" activeClassName="on" to={`${url}/shop`}>
+                        <button
+                            id="buttonShop"
+                        >
+                        </button>
+                    </NavLink>
+                    <NavLink className="off" activeClassName="on" to={`${url}/elements`}>
+                        <button
+                            id="buttonBag"
+                        >
+                        </button>
+                    </NavLink>
+                </GameButtons>
+                <MainSection>
+                    <HeaderStyled>
+                        <Tokens>
+                            <img src={Gold} width="25" height="25" alt="" />
+                            &nbsp;100
+                        </Tokens>
+                        <Tokens>
+                            <img src={Wood} width="25" height="25" alt="" />
+                            &nbsp;200
+                        </Tokens>
+                        <Tokens>
+                            <img src={Gems} width="25" height="25" alt="" />
+                            &nbsp;20
+                        </Tokens>
+                    </HeaderStyled>
+                    <Switch>
+                        <Route exact path={path}>
+                            <h1>Start playing</h1>
+                        </Route>
+                        <Route path={`${path}/:slide`} component={Slide} />
+                    </Switch>
+                    <Switch>
+                        <Route path={`${path}/castle`} component={Castle} />
+                        <Route path={`${path}/shop`} component={Shop} />
+                        <Route path={`${path}/elements`} component={Elements} />
+                    </Switch>
+
+                </MainSection>
+            </GameStyled>
+            <Footer />
+        </>
     );
 };
 
